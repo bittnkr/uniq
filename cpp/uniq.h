4abee4353e17db6dfeda1d9b22bc8b78229038ba
A Bittnkr <bittnkr@gmail.com>
2020-04-01 21:45:30 -0300

    stop signal on Queue

--- cpp/uniq.h
+++ cpp/uniq.h
@@ -1,40 +1,46 @@
-// uniQ The Lock free Queue. 
+// uniQ The Lock free Queue.
 // See test.cpp for use case & README.md for details about the solution
+// Released under LGPL 3.0
 
 #pragma once
-#include <atomic>
 #include <assert.h>
+
+#include <atomic>
 #include <thread>
 #include <vector>
 using namespace std;
 
-template <class T> class Queue {
-protected:
+template <class T>
+class Queue {
+ protected:
   vector<T> buffer;
   int mask;
 
   atomic<int> in, out;
   vector<int> isFree;
 
-public:
+ public:
   Queue(int size_ = 64) : mask(size_) {
-    assert(mask && !(mask & (mask - 1)) && "Queue size must be a positive power of 2.");
+    assert(mask && !(mask & (mask - 1)) &&
+           "Queue size must be a positive power of 2.");
     buffer = vector<T>(mask, 0);
     isFree = vector<int>(size_, 1);
     mask--;
 
     out = in = 0;
+    stop = 0;
   }
 
-  int push(T item) {
+  int stop;
 
+  int push(T item) {
     int i;
     do {
       i = in;
-      while (i - out > mask)
-        sched_yield(); // if full, wait for space
+      if (stop) return 0;
+      while (i - out > mask && !stop) sched_yield();  // if full, wait for space
     } while (!isFree[i & mask] || !in.compare_exchange_weak(i, i + 1));
-     
+
     buffer[i & mask] = item;
     isFree[i & mask] = 0;
     return i;
@@ -43,15 +49,19 @@ public:
   T pop() {
     int o;
     do {
+      if (stop) return T{};
       o = out;
-      while (o == in)
-        sched_yield(); // if empty, wait for item
+      while (o == in && !stop) sched_yield();  // if empty, wait for item
     } while (isFree[o & mask] || !out.compare_exchange_weak(o, o + 1));
 
     T r = buffer[o &= mask];
     isFree[o] = 1;
     return r;
   }
-};
 
-// Released under GNU 3.0
+  // virtual bool allow(int i) { return i; };
+  int size() { return mask + 1; }
+  inline bool full() { return (in - out) > mask; }
+  inline bool empty() { return out == in; }
+  int taskId() { return out; };
+};
