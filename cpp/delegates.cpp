172c9c84c4497de0db25156aa4f4f2895b475c33
A Bittnkr <bittnkr@gmail.com>
2020-04-07 18:40:36 -0300

    tests with function delegation run()

--- cpp/delegates.cpp
+++ cpp/delegates.cpp
@@ -17,8 +17,8 @@
 using namespace std;
 
 // passing a fuction call forward
-u64 spiral(u64 n, u64 min, u64 max) {
-  printf("\nspiral(n:%llu, min:%llu, max:%llu)\n", n, min, max);
+int spiral(int n, int min, int max) {
+  printf("\nspiral(n:%d, min:%d, max:%d)\n", n, min, max);
   return n;
 }
 
@@ -40,38 +40,57 @@ auto run3(Ts&&... ts) {
                   make_index_sequence<sizeof...(Ts) - 1>{});
 }
 
-// // template <typename Func, size_t... Ts2>
-// template <typename Func, typename... Args>
-// auto run4(Func&& f, Args&&... args) {
-//   return combine3(forward_as_tuple(forward<Args>(args)...),
-//                   make_index_sequence<sizeof...(Args) - 1>{});
-// }
+// template <typename Func, size_t... Ts2>
+template <typename TupT, size_t... Is>
+int combine4(TupT&& tup, index_sequence<Is...>) {
+  return get<sizeof...(Is)>(tup)(get<Is>(forward<TupT>(tup))...);
+}
+
+template <typename Func, typename... Args>
+int run4(Func&& f, Args&&... args) {
+  // Job job = bind(forward<Func>(f), forward<Args>(args)...);
+  // return Q.push(job);
+
+  // args.pop_back();
+  // f(&args...);
+  // Job job = bind(forward<Func>(f), forward<Args>(args)...);
+  // job();
+  auto iseq = make_index_sequence<sizeof...(Args) - 1> {};
+  int cb = combine4(forward_as_tuple(forward<Args>(args)...), iseq);
+
+  // return Q.push(job);
+  return 0;
+}
+
+// Some different ways to delegate a function
 
 int main() {
   printf("Press ctrl+c to stop.\n");
 
-  u64 result = 0;
+  int result = 0;
   run(spiral, 1, 2, 3);
   run2(spiral, 1, 2, 3);
 
   //  calling a lambda as first param
-  auto lambda = [&](u64 a, u64 b, u64 c) {
+  auto lambda = [&](int a, int b, int c) {
     ostringstream ss;
-    printf("\nrun2(lambda,%llu,%llu,%llu)\n", a, b, c);
+    printf("\nrun2(lambda,%d,%d,%d)\n", a, b, c);
     return 1;
   };
   run2(lambda, 1, 2, 3);
 
   // lambda as last parameter
-  run3(4, 4, 4, lambda);
+  run3(3, 3, 3, lambda);
 
   // immediate lambda as last parameter
-  run3(5, 5, 5, [&](u64 a, u64 b, u64 c) {
-    return printf("\nrun2(lambda,%llu,%llu,%llu)\n", a, b, c);
+  run3(4, 4, 4, [&](int a, int b, int c) {
+    return printf("\nrun2(lambda,%d,%d,%d)\n", a, b, c);
   });
 
-  // run(spiral, 1, 2, [&](u64 res) { result = res; });
-  //   // run(n, b, b + blockSize, [&](u64 res) {
-  // run(spiral2, 1, 2, 3);
+  // run4(spiral, 1, 2, 3);
+  run4(spiral, 5, 5, 5, lambda);
+
+  // run4(spiral, 7, 7, 7, [&](int res) { result = res; });
+  // run(n, b, b + blockSize, [&](int res) {
   pool.stop();
 }
