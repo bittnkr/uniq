4c76b0bccac1437257589ccaaf7b7f01d7382d4c
bittnkr <bittnkr@gmail.com>
2021-04-02 02:24:44 -0300

    Id() with dictionary

--- cpp/lib/Id.h
+++ cpp/lib/Id.h
@@ -2,26 +2,39 @@
 // Id • A class with id
 //==============================================================================
 #pragma once
+#include "uniq.h"
 namespace uniq {
-
 //======================================================================= Id
-class Id {
-public:
-  inline static Atomic<int> nextId{0};
-  int id;
-  
-  Id(){ id = ++nextId; };
+struct Id {
+  static map<string, Atomic<integer>> Ids;
+  static mutex mutexIds;
 
+  integer id;
+  Id(string group = ""){ 
+    lock_guard<mutex> lock(Id::mutexIds); // Mutex(Ids);
+    auto r = &Ids[group];
+    id = ++(*r);
+    log(group, id);
+  };
+
+  inline operator integer() const { return id; }
+  
   bool operator<(const Id &other) const { return id < other.id; };
   bool operator>(const Id &other) const { return id > other.id; };
   bool operator==(const Id &other) const { return id == other.id; };
 };
 
+map<string, Atomic<integer>> Id::Ids = {};
+mutex Id::mutexIds = {};
+
+// #define ID(x) Id(#x);
+
 //========================================================================= test
-#include "test.h"
 void test_Id(){
-  Id a, b;
+  Id a("a"), b("a"), c;
   CHECK(b.id > 0);
-  CHECK(a.id < b.id);
+  CHECK(b > 0);
+  CHECK(a < b);
+  CHECK(c.id == 1);
 }
 }// uniq • Released under GPL 3.0
