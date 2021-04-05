0f8d271a58a16e3841685cb48c8a8581a049d9f4
bittnkr <bittnkr@gmail.com>
2021-04-05 07:58:01 -0300

    refactoring

--- cpp/lib/uniq.h
+++ cpp/lib/uniq.h
@@ -2,10 +2,11 @@
 #include "std.h" // all std headers
 #include "unistd.h" // posix functions like usleep
 
-#include "test.h" // unit testing
-#include "numtypes.h" // basic numeric types
 #include "terminal.h" // terminal printing with colors
-#include "utils.h" // sstr, format, join, repeat, trim ...
+#include "test.h" // unit testing
+#include "terminal.test.h" // terminal tests, after test.h
+#include "numtypes.h" // basic numeric types
+#include "utils.h" // rnd, format, join, repeat, trim ...
 #include "call.h" // lazy call & lambda utilities
 #include "fs.h" // filesystem utilities readFile, saveFile() ...
 #include "sha256.h" // sha256 cryptographic function
@@ -14,15 +15,16 @@
 #include "Atomic.h" // basic atomic types
 #include "Named.h" // class with name
 #include "Id.h" // incremental id
-#include "Node.h" // parent/children node using shared_ptr
+// #include "model.h" // UniQ classes mockup
 #include "Actor.h" // common parent for active classes
 #include "queue.h" // the queue
 #include "pool.h" // thread pool
 #include "ElasticQueue.h" // a locking queue with elastic storage
+#include "Log.h" // simple logger
 #include "Profiler.h" // execution time recording
 #include "Benchmark.h" // speed tests
-#include "Log.h" // simple logger
-#include "Worker.h" // worker thread
+#include "Node.h" // parent/children node using shared_ptr
+// #include "Worker.h" // worker thread
 // #include "WorkerPool.h" // A Worker with helpers
 // #include "Json.h" // Json primitive
 // #include "Digit.h" // big digit 
